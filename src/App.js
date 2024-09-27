bcd8c7b52cc59479ffb8602f240244f1a5a012b5
agpaknazli <agpaknazli19@gmail.com>
2024-09-27 19:04:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+// index.html e de bootstrap in js teşekkürünü script olarak ekledik
+import Clock from "./Clock"
+import Home from "./pages/Home";
+
+
+
+
+function App() {
+  return (
+    <div >
+ <Clock/>
+ 
+    </div>
+  );
+}
+
+export default App;
